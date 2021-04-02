a844d14fec0ca7504b9f8cfaf816cdb54c15bb2a
Antonio <atcarter005@gmail.com>
2021-04-02 08:44:19 -0500

    commit tree acb0045496d49078576202a241b57cd06598b8cf

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,16 +6,36 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    # @pet = Pet.create(params[name: params["pet[name]", owner_id: params["pet[owner_id]"])
+    # binding.pry
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"])
+      @pet = Pet.create(name: params[:pet][:name], owner_id: @owner.id)
+      @owner.pets << @pet
+    else
+      @owner = Owner.find_by_id(params["pet"]["owner_id"])
+      @pet = Pet.create(name: params[:pet][:name], owner_id: @owner.id)
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
+    # end
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owners.all
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    @owner = Owner.find(@pet.owner_id)
+    @owners = Owner.all
     erb :'/pets/show'
   end
 
